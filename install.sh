34c1e8c8304c3e17a2c722b7958f1e45db8f2666
Josh Heinrichs <joshiheinrichs@gmail.com>
2024-12-22 10:52:21 -0600

    Update config via rsync

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 mkdir -p ~/.local/state/nix/profiles
 nix-env -f packages.nix -iA packages --profile ~/.local/state/nix/profiles/josh
 
-cp bashrc ~/.bashrc
-cp -r bashrc.d ~/.bashrc.d
+rsync --verbose --human-readable --archive --delete bashrc ~/.bashrc
+rsync --verbose --human-readable --archive --delete bashrc.d/ ~/.bashrc.d/
 
-cp -r config/git ~/.config/git
-cp -r config/atuin ~/.config/atuin
+rsync --verbose --human-readable --archive --delete config/git/ ~/.config/git/
+rsync --verbose --human-readable --archive --delete config/atuin/ ~/.config/atuin/
