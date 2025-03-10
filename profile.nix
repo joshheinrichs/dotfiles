26a12855cd7d1adeed853d996c2bcfa3611fe397
Josh Heinrichs <joshiheinrichs@gmail.com>
2025-03-09 21:37:53 -0600

    Clean up package scoping

--- profile.nix
+++ profile.nix
@@ -4,25 +4,25 @@ pkgs.buildEnv {
   name = "dotfiles";
   paths = with pkgs; [
     # avoid system packages for the most part
-    pkgs.coreutils
-    pkgs.bash
-    pkgs.neovim
-    pkgs.git
-    pkgs.atuin
-    pkgs.bash-preexec
-    pkgs.alacritty
-    pkgs.zellij
-    pkgs.fzf
-    pkgs.zoxide
-    pkgs.ripgrep
-    pkgs.bat
-    pkgs.htop
+    coreutils
+    bash
+    neovim
+    git
+    atuin
+    bash-preexec
+    alacritty
+    zellij
+    fzf
+    zoxide
+    ripgrep
+    bat
+    htop
     # desktop junk
-    pkgs.firefox
-    pkgs.vlc
-    pkgs.spotify
-    pkgs.keepassxc
+    firefox
+    vlc
+    spotify
+    keepassxc
     # wayland screen sharing support
-    pkgs.discord-canary
+    discord-canary
   ];
 }
